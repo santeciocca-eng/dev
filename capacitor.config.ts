ad600c1eec56562353a77a288f2dbcb2605d6dcc
santeciocca-eng <sante.ciocca@gmail.com>
2026-09-15 15:18:22 +0200

    Configure Capacitor Android and iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'it.santeciocca.devapp',
+  appName: 'Dev App',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
